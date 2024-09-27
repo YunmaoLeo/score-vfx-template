f182467e0553e41657be3028eb3d28e7c0df4ce6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-09-26 20:05:34 -0400

    [core] Update with score

--- MyVfx/Metadata.hpp
+++ MyVfx/Metadata.hpp
@@ -6,18 +6,18 @@ namespace MyVfx
 class Model;
 }
 
-PROCESS_METADATA(
-    ,
-    MyVfx::Model,
-    "00000000-0000-0000-0000-000000000000",
-    "MyVfx",                           // Internal name
-    "MyVfx",                           // Pretty name
-    Process::ProcessCategory::Visual,  // Category
-    "GFX",                             // Category
-    "My VFX",                          // Description
-    "ossia team",                      // Author
-    (QStringList{"shader", "gfx"}),    // Tags
-    {},                                // Inputs
-    {},                                // Outputs
-    Process::ProcessFlags::SupportsAll // Flags
+PROCESS_METADATA(,
+                 MyVfx::Model,
+                 "00000000-0000-0000-0000-000000000000",
+                 "MyVfx",                           // Internal name
+                 "MyVfx",                           // Pretty name
+                 Process::ProcessCategory::Visual,  // Category
+                 "GFX",                             // Category
+                 "My VFX",                          // Description
+                 "ossia team",                      // Author
+                 (QStringList{"shader", "gfx"}),    // Tags
+                 {},                                // Inputs
+                 {},                                // Outputs
+                 QUrl{},                            // Doc link
+                 Process::ProcessFlags::SupportsAll // Flags
 )
